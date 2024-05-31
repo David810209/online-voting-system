c66f495ef02d121f0e3208db1de1de6a11b3a028
David810209 <a1050120bc@gmail.com>
2024-06-01 06:46:49 +0800

    make it better

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
+enableCORS = true\n\
 port = $PORT\n\
+enableXsrfProtection = false\n\
 " > ~/.streamlit/config.toml
