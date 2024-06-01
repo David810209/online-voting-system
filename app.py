b21de0d25949dd2d9d83de26db91130f4351360f
David810209 <a1050120bc@gmail.com>
2024-06-02 05:21:31 +0800

    test

--- app.py
+++ app.py
@@ -20,11 +20,7 @@ limiter = Limiter(
 
 app.secret_key = FLASK_SECRET_KEY  # 用於會話加密，請更換為更安全的值
 
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         user_name = request.form['username']
@@ -107,27 +103,6 @@ def success():
 @app.route('/haha')
 def haha():
     return render_template('haha.html')
-
-@app.route('/zero')
-def zero():
-    return render_template('0.html')
-
-@app.route('/high')
-def high():
-    return render_template('1.html')
-
-@app.route('/two')
-def two():
-    return render_template('2.html')
-
-@app.route('/three')
-def three():
-    return render_template('3.html')
-
-@app.route('/four')
-def four():
-    return render_template('4.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
