821ff6dea12cd0887869eab17725000ee36fd04b
David810209 <a1050120bc@gmail.com>
2024-06-01 09:58:30 +0800

    make it better

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-
+import base64
 from redis_get.redis_db import RedisHandler
-from encrypt.rsa_process import rsa_handler
+from encrypt.rsa_process import load_private_key, decrypt_data, encrypt_data,generate_rsa_key_pair
 from config import REDIS_HOST,REDIS_PORT,REDIS_PASSWORD
 app = Flask(__name__)
 redis_handler = RedisHandler(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
-rsa_handler = rsa_handler()
+
 app.secret_key = 'supersecretkey'  # 用於會話加密，請更換為更安全的值
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,14 +32,59 @@ def info():
         vice_president_choice = request.form['vice_president']
         print(president_choice, vice_president_choice)
         user_id = session.get('user_id')  # 從會話中獲取用戶 ID
-        encrypted_president_choice = rsa_handler.encrypt(president_choice)
-        encrypted_vice_president_choice = rsa_handler.encrypt(vice_president_choice)
+        public_key,private_key,public_key_pem, private_key_pem = generate_rsa_key_pair()
+        encrypted_president_choice = encrypt_data(public_key,president_choice)
+        encrypted_vice_president_choice = encrypt_data(public_key,vice_president_choice)
         print(encrypted_president_choice, encrypted_vice_president_choice)
-        redis_handler.update_private_key(user_id, encrypted_president_choice, encrypted_vice_president_choice)
+        redis_handler.update_vote(user_id, encrypted_president_choice, encrypted_vice_president_choice)
         flash('投票成功', 'success')
    
 
     return render_template('info.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    decrypted_president_choice = None
+    decrypted_vice_president_choice = None
+
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        private_key_pem = request.form['private_key']
+
+        encrypted_president_choice_b64 = redis_handler.rds.hget(f'voter:{user_id}', 'president')
+        encrypted_vice_president_choice_b64 = redis_handler.rds.hget(f'voter:{user_id}', 'vice_president')
+
+        if not encrypted_president_choice_b64 or not encrypted_vice_president_choice_b64:
+            flash('沒有可顯示的結果', 'error')
+            return redirect(url_for('result'))
+
+        encrypted_president_choice = base64.b64decode(encrypted_president_choice_b64)
+        encrypted_vice_president_choice = base64.b64decode(encrypted_vice_president_choice_b64)
+
+        private_key = load_private_key(private_key_pem.encode('utf-8'))
+
+        # 解密用戶選擇
+        decrypted_president_choice = decrypt_data(private_key, encrypted_president_choice)
+        decrypted_vice_president_choice = decrypt_data(private_key, encrypted_vice_president_choice)
+
+    return render_template('result.html', 
+                           president_choice=decrypted_president_choice, 
+                           vice_president_choice=decrypted_vice_president_choice)
+    
+@app.route('/success')
+def success():
+    user_id = session.get('user_id')
+    private_key_pem = redis_handler.get_private_key(user_id)
+    
+    president_choice = redis_handler.get_president_choice(user_id)
+    vice_president_choice = redis_handler.get_vice_president_choice(user_id)
+
+    return render_template('success.html',
+                           private_key_pem=private_key_pem,
+                           president_choice=president_choice,
+                           vice_president_choice=vice_president_choice)
+        
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- redis_get/redis_db.py
+++ redis_get/redis_db.py
@@ -9,8 +9,12 @@ class RedisHandler():
         self.rds.hmset(f'voter:{user_id}',{
             "user_name":user_name,
             'user_id':user_id,
-            "president_private_key":"",
-            "vice_president_private_key":""
+            'public_key':"",
+            "private_key":"",
+            "president":"",
+            "vice_president":"",
+            "president_text":"",
+            "vice_president_text":"",
         })
         
     def get_user_name(self,user_id):
@@ -19,16 +23,30 @@ class RedisHandler():
     def get_user_id(self,user_id):
         return self.rds.hget(f'voter:{user_id}','user_id')
     
+    def get_public_key(self,user_id):
+        return self.rds.hget(f'voter:{user_id}','public_key')
+
     def get_private_key(self,user_id):
         return self.rds.hget(f'voter:{user_id}','private_key')
+
+    def get_president_choice(self,user_id):
+        return self.rds.hget(f'voter:{user_id}','president_text')
+
+    def get_vice_president_choice(self,user_id):
+        return self.rds.hget(f'voter:{user_id}','vice_president_text')
     
+    def store_key(self,user_id,public_key, private_key):
+        self.rds.hset(f'voter:{user_id}','public_key',public_key)
+        self.rds.hset(f'voter:{user_id}','private_key',private_key)
+        
     def user_exists(self, user_id):
         return self.rds.exists(f'voter:{user_id}')
     
-    def update_private_key(self, user_id, president_choice, vice_president_choice):
+    def update_vote(self, user_id, president_choice, vice_president_choice):
         self.rds.hmset(f'voter:{user_id}', {
-            'president_private_key': president_choice,
-            'vice_president_private_key': vice_president_choice
+            'president': president_choice,
+            'vice_president': vice_president_choice
         })
+    
         
    
