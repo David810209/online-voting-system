5433e736512b8c58870520c69b6749f0b6ab4fd0
David810209 <a1050120bc@gmail.com>
2024-06-11 21:53:11 +0800

    update_key_manage

--- app.py
+++ app.py
@@ -101,18 +101,22 @@ def check():
 
 
 
-@app.route('/getkey', methods=['POST'])
+@app.route('/getkey', methods=['GET', 'POST'])
 def getkey():
-    user_id = request.form['user_id']
-    try:
-        public_key = redis_handler.get_public_key(user_id)
-        private_key = redis_handler.get_private_key(user_id)
-        if not public_key or not private_key:
-            error_message = "找不到指定的鍵"
-            return render_template("getkey.html", error=error_message)
-        return render_template("getkey.html", private_key=private_key, public_key=public_key, user_id=user_id)
-    except Exception as e:
-        return render_template("getkey.html", error=str(e))
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        try:
+            public_key = redis_handler.get_public_key(user_id)
+            private_key = redis_handler.get_private_key(user_id)
+            if not public_key or not private_key:
+                error_message = "找不到指定的鍵"
+                return render_template("getkey.html", error=error_message)
+            return render_template("getkey.html", private_key=private_key, public_key=public_key, user_id=user_id)
+        except Exception as e:
+            return render_template("getkey.html", error=str(e))
+    else:
+        # 處理 GET 請求的邏輯
+        return render_template("getkey.html")
     
 @app.route('/success')
 @login_required
