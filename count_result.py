2ecd93c71221e174f3f6cdfebfa367043b49a1f7
David810209 <a1050120bc@gmail.com>
2024-06-13 17:36:53 +0800

    update front end

--- count_result.py
+++ count_result.py
@@ -2,10 +2,7 @@ import os
 import base64
 from dotenv import load_dotenv
 from redis_get.redis_db import RedisHandler
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
-
+from encrypt.rsa_process import  decrypt_data
 load_dotenv()
 REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = int(os.getenv('REDIS_PORT'))
@@ -20,26 +17,7 @@ results = []
 president_votes = {}
 vice_president_votes = {}
  
-def load_private_key(filepath):
-    with open(filepath, "rb") as key_file:
-        private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=None,
-        )
-    return private_key
 
-def decrypt_data(private_key, ciphertext):
-    decrypted_data = private_key.decrypt(
-        ciphertext,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return decrypted_data
-
-private_key = load_private_key("private_key.pem")
 
 for voter_key in voters:
     user_id = voter_key.split(':')[1]
@@ -55,23 +33,24 @@ for voter_key in voters:
         if president_ciphertext:
             ciphertext = base64.b64decode(president_ciphertext)
             print(f"Decoded president ciphertext: {ciphertext}")
-            president_choice = decrypt_data(private_key, ciphertext).decode('utf-8')
+            private_key = redis_handler.get_private_key(user_id)
+            president_choice = decrypt_data(ciphertext, private_key)
         else:
             president_choice = None
 
         if vice_president_ciphertext:
             ciphertext_2 = base64.b64decode(vice_president_ciphertext)
             print(f"Decoded vice_president ciphertext: {ciphertext_2}")
-            vice_president_choice = decrypt_data(private_key, ciphertext_2).decode('utf-8')
+            vice_president_choice = decrypt_data(ciphertext_2, private_key)
         else:
             vice_president_choice = None
-
-        results.append({
-            'username': username,
-            'userid': user_id,
-            'president': president_choice,
-            'vice_president': vice_president_choice
-        })
+        if president_choice and vice_president_choice:
+            results.append({
+                'username': username,
+                'userid': user_id,
+                'president': president_choice,
+                'vice_president': vice_president_choice
+            })
 
         if president_choice:
             president_votes[president_choice] = president_votes.get(president_choice, 0) + 1
